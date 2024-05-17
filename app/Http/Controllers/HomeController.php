ca6ea5fbdbff0e3bb7b5264068f1539e66a29d2b
dgonzalezi <dgonzalezi@eafit.edu.co>
2024-05-17 15:56:28 -0500

    Consumo de API añadido

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\View\View;
 
 class HomeController extends Controller
@@ -9,6 +11,15 @@ class HomeController extends Controller
     {
         $viewData = [];
         $viewData["title"] = trans("app.home_controller.title");
+
+        $response = Http::get('https://api.adviceslip.com/advice');
+
+        if ($response->successful()) {
+            $viewData['advice'] = $response->json()['slip']['advice'];
+        } else {
+            $viewData['advice'] = "No advice available at the moment.";
+        }
+
         return view('home.index')->with("viewData", $viewData);
     }
 }
