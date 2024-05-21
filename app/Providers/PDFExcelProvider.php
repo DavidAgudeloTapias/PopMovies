ff9e652b052f0a5e4d547980758a546f546070d1
majogonzalezp <mjgonzalep@eafit.edu.co>
2024-05-20 20:34:29 -0500

    Provider de ID añadido

---
+++ app/Providers/PDFExcelProvider.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use app\Interfaces\ReportGeneratorInterface;
+use App\Util\PdfReportGenerator;
+use App\Util\ExcelReportGenerator;
+
+class PDFExcelProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->bind(ReportGeneratorInterface::class, function() {
+            return new PdfReportGenerator();
+        });
+
+        $this->app->bind(ReportGeneratorInterface::class, function() {
+            return new ExcelReportGenerator();
+        });
+    }
+}
