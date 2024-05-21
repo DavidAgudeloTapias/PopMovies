d97a1dc32c4ea33f50d58b911302312e4a636246
dgonzalezi <dgonzalezi@eafit.edu.co>
2024-05-20 20:48:18 -0500

    Configuracion del controlador para ID añadida

--- app/Http/Controllers/MyAccountController.php
+++ app/Http/Controllers/MyAccountController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Util\PdfReportGenerator;
+use App\Util\ExcelReportGenerator;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class MyAccountController extends Controller
 {
@@ -15,5 +18,38 @@ class MyAccountController extends Controller
         $viewData["subtitle"] = trans("app.account_controller.subtitle");
         $viewData["orders"] = Order::with(['items.movie'])->where('user_id', Auth::user()->getId())->get();
         return view('myaccount.orders')->with("viewData", $viewData);
+    }
+
+    protected $pdfReportGenerator;
+    protected $excelReportGenerator;
+
+    public function __construct(PdfReportGenerator $pdfReportGenerator, ExcelReportGenerator $excelReportGenerator)
+    {
+        $this->pdfReportGenerator = $pdfReportGenerator;
+        $this->excelReportGenerator = $excelReportGenerator;
+    }
+
+    public function generatePdfReport(int $orderId) : Response
+    {
+        $order = Order::with('items.movie')->findOrFail($orderId);
+        $data = $order->toArray();
+
+        $filePath = $this->pdfReportGenerator->generate($data);
+
+        return response()->download($filePath, 'order_' . $orderId . '.pdf', [
+            'Content-Type' => 'application/pdf',
+        ]);
+    }
+
+    public function generateExcelReport(int $orderId) : Response
+    {
+        $order = Order::with('items.movie')->findOrFail($orderId);
+        $data = $order->toArray();
+
+        $filePath = $this->excelReportGenerator->generate($data);
+
+        return response()->download($filePath, 'order_' . $orderId . '.xlsx', [
+            'Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        ]);
     }
 }
